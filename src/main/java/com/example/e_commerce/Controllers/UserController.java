d86086a74c3099936da272f2c64d1b19dff3e565
maram hassen <maram.hassen@esprit.tn>
2026-02-03 01:25:09 +0100

    add webconfig and CrossOrigin

--- src/main/java/com/example/e_commerce/Controllers/UserController.java
+++ src/main/java/com/example/e_commerce/Controllers/UserController.java
@@ -3,12 +3,14 @@ package com.example.e_commerce.Controllers;
 import com.example.e_commerce.Entities.User;
 import com.example.e_commerce.Services.IUserService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
 @CrossOrigin(origins = "http://localhost:4200")
+
 public class UserController {
     private final IUserService userService;
 
